6b3670f82033e10152906937973e5a9925f8b8d1
Plamen Ivanov - LT <plamko@abv.bg>
2017-11-22 18:59:20 +0200

    php exercices

--- Software Technology/13-PHP-SYNTAX AND BASIC WEB-LAB/09-Capital-Case Words.php
+++ Software Technology/13-PHP-SYNTAX AND BASIC WEB-LAB/09-Capital-Case Words.php
@@ -1,17 +1,17 @@
-<?php
-if (isset($_GET['text'])) {
-	$text = $_GET['text'];
-	preg_match_all('/\w+/', $text, $words);
-	$words = $words[0];
-	$upperWords = array_filter($words, function($word){
-		return strtoupper($word) == $word;
-	});
-	$upper = implode(', ', $upperWords);
-}
-?>
+
 <form>
 	<textarea name="text" cols="30" rows="10"><?= $upper?></textarea>
 	<br/>
 	<input type="submit">
 </form>
-
+<?php
+if (isset($_GET['text'])) {
+    $text = $_GET['text'];
+    preg_match_all('/\w+/', $text, $words);
+    $words = $words[0];
+    $upperWords = array_filter($words, function($word){
+        return strtoupper($word) == $word;
+    });
+    $upper = implode(', ', $upperWords);
+}
+?>
