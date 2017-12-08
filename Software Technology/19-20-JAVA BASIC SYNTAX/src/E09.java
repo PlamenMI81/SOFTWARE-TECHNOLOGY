1d45b78db18dd647f32595655dae5223d761abeb
PlamenMIvanov <plamko@abv.bg>
2017-12-08 02:56:48 +0200

    JAVA BLOG

--- Software Technology/19-20-JAVA BASIC SYNTAX/src/E09.java
+++ Software Technology/19-20-JAVA BASIC SYNTAX/src/E09.java
@@ -1,14 +1,21 @@
 import java.util.*;
-import java.util.function.BinaryOperator;
-
-//09. Most Frequent Number
+//09. Most Frequent Number -> 80%
 public class E09 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        HashMap<Integer,Integer> nums=new HashMap<>();
         int[] numArr = Arrays
                 .stream(scanner.nextLine().split(" "))
                 .mapToInt(Integer::parseInt)
                 .toArray();
-
+        for (Integer element : numArr) {
+            if (nums.containsKey(element)) {
+                nums.put(element, nums.get(element) + 1);
+            } else {
+                nums.put(element, 1);
+            }
+        }
+        Integer key = Collections.max(nums.entrySet(), Map.Entry.comparingByValue()).getKey();
+        System.out.println(key);
     }
 }
